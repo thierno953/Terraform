95f0b4f76443df30190a0a250f9e21d0c9198381
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-22 16:27:11 +0200

    variables input

--- variable-input/main.tf
+++ variable-input/main.tf
@@ -9,18 +9,15 @@ resource "aws_instance" "example_instance" {
   
 variable "ec2_region" {
   type        = string 
-  default     = "eu-west-3"
   description = "Specify the region where the resource will be created"
 }
 
 variable "ec2_instance_type" {
   type        = string 
-  default     = "t2.micro"
   description = "Instance type to launch the instance"
 }
 
 variable "ec2_image_id" {
   type        = string 
-  default     = "ami-xxxxxxxxxxxxxxx"
   description = "AMI to use when launching the instance"
 }
