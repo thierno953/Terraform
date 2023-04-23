f5e1663106e97fb174daae90eaf5ef7bfe4a0ec6
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-23 02:19:56 +0200

    remote-exec provisioner

--- provisioners/remote-exec/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_key_pair" "existing_key_pair" {
-  key_name = var.ec2_key_name
-}
--- provisioners/remote-exec/script.sh
+++
@@ -1,1 +0,0 @@
-<h1>This file was created on the terraform machine and copied to the new EC2 instance</h1>
