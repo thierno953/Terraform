aba989cabdb53855206b68a4733f53f75a67c175
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-23 02:13:27 +0200

    file provisioner

---
+++ provisioners/remote-exec/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web_instance" {
+  instance_type = var.ec2_instance_type
+  ami           = var.ec2_ami
+  key_name      = "${data.aws_key_pair.existing_key_pair.key_name}"
+  tags          = local.common_tags
+
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/home/ubuntu/script.sh"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("/home/ubuntu/remote_exec/new_key.pem")}"
+      host        = "${aws_instance.web_instance.public_ip}"
+      port        = 22 
+    }
+  } 
+}
---
+++ provisioners/remote-exec/variables.tf
@@ -0,0 +1,19 @@
+variable "ec2_instance_type" {
+  type = string
+  description = "Instance type to use when launching the instance"
+}
+
+variable "ec2_ami" {
+  type = string
+  description = "AMI to use when launching the instance"
+}
+
+variable "ec2_key_name" {
+  type = string
+  description = "Key pair to attach to the ec2 instance"
+}
+
+variable "ec2_region" {
+  type = string
+  description = "Region to create the resoures"
+}
