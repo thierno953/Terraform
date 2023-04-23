ad9c0f4dcc2402989c8ab3410c77020d2b91364a
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-23 12:12:58 +0200

    lifecycle

--- meta-arguments /lifecycle/main.tf
+++ meta-arguments /lifecycle/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "lifecycle_example" {
-    ami           = var.ec2_image
-    instance_type = var.ec2_instance_type
+  ami           = var.ec2_image
+  instance_type = var.ec2_instance_type
 
-    tags          = local.common_tags
+  tags          = local.common_tags
 
-    lifecycle {
-      create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
