de06ccdc25b34d8660595ac2ca5a02dea213dcf0
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-23 02:19:20 +0200

    remote-exec provisioner

--- provisioners/remote-exec/variables.tf
+++ provisioners/remote-exec/variables.tf
@@ -1,19 +1,19 @@
-variable "ec2_instance_type" {
-  type = string
+variable "ec2_key_name" {
+  description = "Key name to use when launching the instance"
+  type        = string
+}
+
+variable  "ec2_instance_type" {
   description = "Instance type to use when launching the instance"
+  type        = string
 }
 
 variable "ec2_ami" {
-  type = string
   description = "AMI to use when launching the instance"
-}
-
-variable "ec2_key_name" {
-  type = string
-  description = "Key pair to attach to the ec2 instance"
+  type        = string
 }
 
 variable "ec2_region" {
-  type = string
-  description = "Region to create the resoures"
+  description = "Specify the region to create the resources"
+  type        = string
 }
