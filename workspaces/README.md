90322bb4db470f7987625b6c18e08c0879c0ba80
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-23 14:38:42 +0200

    workspaces

---
+++ workspaces/README.md
@@ -0,0 +1,25 @@
+# Syntax 👍
+
+```bash
+terraform workspace <subcommand> [options] [args]
+
+terraform workspace list [DIR]
+
+terraform workspace new [DIR]
+
+terraform workspace select [DIR]
+
+terraform workspace delete [DIR]
+
+terraform workspace show [DIR]
+
+# example
+
+terraform workspace new dev
+
+terraform workspace select dev
+
+terraform workspace delete dev
+
+terraform workspace show 
+```
---
+++ workspaces/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+ region  = var.region
+}
+
+resource "aws_instance" "this" {
+  count                  = var.instance_count
+  instance_type          = var.instance_type
+  ami                    = var.image_id
+  key_name               = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+}
+
+resource "aws_security_group" "allow_http" {
+  name                   = var.name
+  description            = var.name
+  vpc_id                 = "vpc-xxxxxxxxxxxxxxxxx"
+
+  ingress {
+    description          = "Allow http access"
+    from_port            = 80
+    to_port              = 80
+    protocol             = "tcp"
+    cidr_blocks          = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description          = "Allow ssh access"
+    from_port            = 22
+    to_port              = 22
+    protocol             = "tcp"
+    cidr_blocks          = ["0.0.0.0/0"]
+  }
+}
