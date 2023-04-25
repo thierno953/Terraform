3b196abc0b449aae2933b18ba36bf4a5da0d9c47
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-25 19:22:51 +0200

    deleted

--- resources/main.tf
+++
@@ -1,5 +0,0 @@
-        #resource type    #local name
-resource "aws_instance" "example_instance" {  
-  ami           = "ami-xxxxxxxxxxxxxxx" # Public IP
-  instance_type = "t2.micro"            # Instance Id
-}
