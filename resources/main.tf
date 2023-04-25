f45598603dee6b07be745fc9d3373047155368e2
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-25 19:05:12 +0200

    resources

--- resources/main.tf
+++ resources/main.tf
@@ -1,15 +1,5 @@
-provider "aws" {
-   region     = "eu-west-3"
-   access_key = "my-access-key"
-   secret_key = "my-secret-key"
-}
-
-         #type           #local name
+        #resource type    #local name
 resource "aws_instance" "example_instance" {  
   ami           = "ami-xxxxxxxxxxxxxxx" # Public IP
   instance_type = "t2.micro"            # Instance Id
-
-  tags = {
-        Name = "Terraform EC2"          # Tags
-  }
 }
