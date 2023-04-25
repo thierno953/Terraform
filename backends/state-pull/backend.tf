a3bf165ed0b528af2ba11e3252899db7e370a50d
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-04-25 08:50:58 +0200

    viewing state file

---
+++ backends/state-pull/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "dgruploads-terraform-state-file"
+    key    = "terraform_state_file/terraform.tfstate"
+    region = "eu-west-3"
+  }
+}
+
+# terraform state pull
