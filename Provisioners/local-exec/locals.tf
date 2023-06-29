7c427596b0c9934f416bd1c3eafbddfb381eb3f8
thierno <77080504+thierno953@users.noreply.github.com>
2023-06-29 19:30:33 +0200

    local-exec

---
+++ Provisioners/local-exec/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  common_tags = {
+    Name    = "local_exec_example"
+    Project = "Local_exec_project"
+  }
+}
---
+++ Provisioners/local-exec/resource.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "local_exec_example" {
+  instance_type = var.ec2_instance_type
+  ami           = var.ec2_image 
+  tags          = local.common_tags
+
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_IPs.txt"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_IPs.txt"
+  }
+}
