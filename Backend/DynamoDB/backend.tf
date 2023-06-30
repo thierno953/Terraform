2d46737e5ef4b7599dbb06f0ff8f085a57e0a78f
thierno <77080504+thierno953@users.noreply.github.com>
2023-06-30 08:58:51 +0200

    DynamoDB

--- Backend/DynamoDB/backend.tf
+++ Backend/DynamoDB/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "thiernos-terraform-state-file"
     key            = "terraform_state_file/terraform.tfstate"
-    encrypt        = true
+    encrypt        = true 
     region         = "eu-west-3"
     dynamodb_table = "terraform_state_file_lock"
   }
