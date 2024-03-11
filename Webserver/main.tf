f442241d9162020e47c2f6139f1bdd13b6d8c896
thierno953 <77080504+thierno953@users.noreply.github.com>
2024-03-11 21:27:58 +0100

    web server

---
+++ Webserver/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "tfVPC" {
+  source = "./modules/vpc"
+
+  vpc_cidr             = local.vpc_cidr
+  vpc_tags             = var.vpc_tags
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
+}
+
+module "webserver" {
+  source = "./modules/webserver"
+
+  tf_vpc_id         = module.tfVPC.vpc_id
+  tf_public_subnets = module.tfVPC.public_subnets
+}
---
+++ Webserver/modules/webserver/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  ami_id        = "ami-0752a70c4669a5675"
+  instance_type = "t2.micro"
+  key_name      = "linuxvm"
+}
